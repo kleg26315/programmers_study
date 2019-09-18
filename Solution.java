7d1bd20568de9ac3b046f1b69afc9a1be13c4bc0
김경섭(Kim KyouongSub) <kleg26315@naver.com>
2019-09-18 22:50:32 +0900

    같은 숫자는 싫어

--- Solution.java
+++ Solution.java
@@ -1,34 +1,45 @@
 /*
-2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 
-두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, 
-solution을 완성하세요. 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT입니다.
-예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 TUE를 반환하세요.
+배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다. 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다. 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
 
-제한 조건
-2016년은 윤년입니다.
-2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
+arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+배열 arr의 크기 : 1,000,000 이하의 자연수
+배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
 */
 package codingtest;
 
-public class Solution {
-  public String solution(int a, int b) {
-	  String[] days = {"THU","FRI","SAT","SUN","MON","TUE","WEN"};
-	  int[] months = {31,29,31,30,31,30,31,31,30,31,30,31};
-      String answer = "";
+import java.util.ArrayList;
+import java.util.List;
 
-	  for(int i =0; i<a-1; i++) {
-		  b += months[i];
-	  }
-	  
-	  answer = days[b%7];
-      return answer;
-  }
+public class Solution {
+	public int[] solution(int []arr) {
+        int[] answer = {};
+        ArrayList numbers =new ArrayList();
+        numbers.add(arr[0]);
+        
+        int m=0;
+        for(int i =0; i<arr.length; i++) {
+        	if(Integer.parseInt(numbers.get(m).toString())!=arr[i]) {
+        		m++;
+        		numbers.add(arr[i]);
+        	}
+        	
+        }
+        answer = new int[numbers.size()];
+        for(int i=0; i<numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+            answer[i] = (int) numbers.get(i);
+        }
+        return answer;
+	}
     
     public static void main(String[] args){
         Solution sol = new Solution();
-        int a = 12;
-        int b = 31;
-        System.out.println(sol.solution(a,b));
+        int[] arr = {1,1,3,3,0,1,1}; 
+        sol.solution(arr);
         
     }
 }
