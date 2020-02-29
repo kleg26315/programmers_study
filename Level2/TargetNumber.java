de8f2459ca103ce1d12f11114be44b65100aecfd
김경섭(Kim KyouongSub) <kleg26315@naver.com>
2020-02-29 12:02:17 +0900

    타겟넘버_DFS/BFS

---
+++ Level2/TargetNumber.java
@@ -0,0 +1,25 @@
+package DFS_BFS;
+
+public class TargetNumber {
+   public static void main(String[] args){
+        int[] numbers = {1,1,1};
+        int target = 1;
+        System.out.println(solution(numbers, target));
+    }
+   
+    public static int solution(int[] numbers, int target) {
+        int answer = 0;
+        answer = dfs(numbers, 0, 0, target);
+        return answer;
+    }
+    
+    private static int dfs(int[] numbers, int node, int sum, int target){
+        if(node == numbers.length){
+            if(sum==target)
+                return 1;
+            return 0;
+        }
+        return dfs(numbers, node+1, sum + numbers[node], target) 
+        	 + dfs(numbers, node+1, sum - numbers[node], target);
+    }
+}
